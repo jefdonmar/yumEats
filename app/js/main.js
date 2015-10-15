c2df9f2848bc4faf696903313c60b3565097d4c1
Cori Fowler <fowler.cori@gmail.com>
2015-10-15 13:31:32 -0400

    Finished with Google API & Blog API - working on Daily Specials API

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,27 @@
 
 (function () {
 
-  console.log('It Works!');
+  // Blog JS Code for "Latest News" section
+
+  var blogUrl = 'https://json-data.herokuapp.com/restaurant/news/1';
+
+  var blogTemplate = _.template($('#blog-post').text());
+
+  var blogPromise = $.getJSON(blogUrl);
+
+  blogPromise.then(function (postInfo) {
+    var blogInfo = blogTemplate(postInfo);
+    $('.latest-news').append(blogInfo);
+  });
+
+  // Special JS Code for "Daily Special" section
+
+  var specialUrl = 'https://json-data.herokuapp.com/restaurant/special/1';
+  var menuUrl = 'https://json-data.herokuapp.com/restaurant/menu/2';
+  var specialTemplate = _.template($('#specials').text());
+
+  var specialPromise = $.getJSON(specialUrl);
+  var menuPromise = $.getJSON(menuUrl);
+
+  Promise.all([specialPromise, menuPromise]).then(function (object) {});
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 (function () {
 
-  console.log('It Works!');
+// Blog JS Code for "Latest News" section
+
+let blogUrl = 'https://json-data.herokuapp.com/restaurant/news/1';
+
+let blogTemplate = _.template($('#blog-post').text());
+
+let blogPromise = $.getJSON(blogUrl);
+
+blogPromise.then( function(postInfo) {
+  let blogInfo = blogTemplate(postInfo);
+  $('.latest-news').append(blogInfo);
+});
+
+// Special JS Code for "Daily Special" section
+
+let specialUrl = 'https://json-data.herokuapp.com/restaurant/special/1';
+let menuUrl = 'https://json-data.herokuapp.com/restaurant/menu/2';
+let specialTemplate = _.template($('#specials').text());
+
+let specialPromise = $.getJSON(specialUrl);
+let menuPromise = $.getJSON(menuUrl);
+
+
+Promise.all([specialPromise, menuPromise]).then(function(object){
+
+});
+
+
 
 }());
+
+
