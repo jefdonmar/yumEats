f0461999c4e2c3fc7925d232f31a29e5c3021594
Isaac Hardy <isaachardyDEV@gmail.com>
2015-10-15 12:20:26 -0400

    Added a set of functions to display the menu items

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,40 @@
 
 (function () {
 
-  console.log('It Works!');
+	// blah
+	var menuUrl = "https://json-data.herokuapp.com/restaurant/menu/2";
+	var promise = $.getJSON(menuUrl);
+	console.log(promise);
+
+	// blah
+	promise.then(function (res) {
+		console.log(res);
+		doSomething(res);
+	});
+
+	// blah
+	var createTemplate = function createTemplate(arr) {
+		console.log(arr);
+		var menuBlock = '';
+
+		arr.forEach(function (option) {
+			menuBlock += '\n      \t\t<h2>' + option.item + '</h2>\n      \t\t<p>' + option.description + '</p>';
+		});
+
+		var finalTemp = '\n\t\t\t<div class="menuBreakfast">\n\t\t\t\t' + menuBlock + '\n\t\t\t</div>\n\t\t';
+		return finalTemp;
+	};
+
+	// blah
+	var doSomething = function doSomething(obj) {
+		var breakfastTemplate = createTemplate(obj.breakfast);
+		var dessertsTemplate = createTemplate(obj.desserts);
+		var drinksTemplate = createTemplate(obj.drinks);
+
+		console.log(breakfastTemplate);
+
+		$('.menu').append(breakfastTemplate);
+		$('.menu').append(dessertsTemplate);
+		$('.menu').append(drinksTemplate);
+	};
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,49 @@
 (function () {
 
-  console.log('It Works!');
+	
+	// blah
+	let menuUrl = "https://json-data.herokuapp.com/restaurant/menu/2";
+	let promise = $.getJSON(menuUrl);
+	console.log(promise);
+
+	// blah
+	promise.then (function (res) {
+		console.log(res);
+		doSomething(res); 
+	}); 
+
+	// blah
+	let createTemplate = function(arr) {
+		console.log(arr);
+		let menuBlock = '';
+
+		arr.forEach(function(option) {
+      		menuBlock += `
+      		<h2>${ option.item }</h2>
+      		<p>${ option.description }</p>`;
+    	});
+
+		let finalTemp =`
+			<div class="menuBreakfast">
+				${menuBlock}
+			</div>
+		`;
+		return finalTemp;
+	}; 
+ 	
+ 	// blah
+ 	let doSomething = function (obj) {
+ 		let breakfastTemplate = createTemplate(obj.breakfast);
+ 		let dessertsTemplate = createTemplate(obj.desserts);
+ 		let drinksTemplate = createTemplate(obj.drinks);
+
+ 		console.log(breakfastTemplate);
+
+ 			$('.menu').append(breakfastTemplate);
+ 			$('.menu').append(dessertsTemplate);
+			$('.menu').append(drinksTemplate);
+
+ 	}; 
+
 
 }());
